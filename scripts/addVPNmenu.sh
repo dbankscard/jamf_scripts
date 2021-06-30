3062954a17a5122e43f11b2ac8e6551e1fef7d41
Dwight <dwight.banks@icloud.com>
2021-06-29 21:38:07 -0500

    changed variable command subsition syntax

--- scripts/addVPNmenu.sh
+++ scripts/addVPNmenu.sh
@@ -6,7 +6,7 @@
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 # Identify the UID of the logged-in user
-user_uid=`id -u "$loggedInUser"`
+user_uid=$(id -u "$loggedInUser")
 
 #add VPN menu item to toolbar
 /bin/launchctl asuser "$user_uid" /usr/bin/open "/System/Library/CoreServices/Menu Extras/VPN.menu"
