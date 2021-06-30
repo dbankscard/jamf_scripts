93464f3d7c97f40ff1c0356ab8033c9e3d11be32
Dwight <dwight.banks@icloud.com>
2021-06-29 21:19:49 -0500

    updated script

--- scripts/removeVPNconfig.sh
+++ scripts/removeVPNconfig.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-#purpose: this script is to be used when an employee separates from the company. this is the first iteration of this script which will remove vpn config profiles from the jamf pro instance by using the employee first and last name. 
+#purpose: this script was created to remove users vpn configuration profiles from the Jamf Pro server after the company has begun the offboarding process.
+#Each vpn config profile name follows this syntax - "VPN - Firstname Lastname". This can placed in Self Service scoped ONLY to the IT department.  
 
 ##API Call Info
 jamfProURL="https://jss.companyname.com/JSSResource"
-#get separated username
-username=$(osascript -e 'tell application "System Events" to text returned of (display dialog "Enter user first and last name." default answer "John Smith" buttons {"Cancel", "OK"} default button 2 with icon {"/usr/local/GHC/GHCicon.png"} with title "Removing User VPN Configuration Profiles"  )' )
 
-# Check status of osascript, if user canceld exit script
+ ##Jamf credentials are encoded using base64
+jamfcreds=$(echo "username:password" | base64 )
+
+#get username
+username=$(osascript -e 'tell application "System Events" to text returned of (display dialog "Enter user first and last name." default answer "John Smith" buttons {"Cancel", "OK"} default button 2 with icon {"/path/to/file.png"} with title "Removing User VPN Configuration Profiles"  )' )
+
+#check status of osascript, if user clicked cancel exit script.. 
 if [ "$?" != "0" ] ; then
 	echo "User aborted. Exiting..."
 	exit 1
 fi
 
 until [[ "$username" =~ ^[A-Z][a-z]+[[:space:]]+[A-Z][a-z]+$ ]]; do
-	username=$(osascript -e 'tell application "System Events" to text returned of (display dialog "You did not enter the first and last name. Enter user first and last name." default answer "John Smith" buttons {"Cancel", "OK"} default button 2 with icon {"/usr/local/GHC/GHCicon.png"})' )
-	# Check status of osascript, if user canceld exit script
+	username=$(osascript -e 'tell application "System Events" to text returned of (display dialog "You did not enter the first and last name. Enter user first and last name." default answer "John Smith" buttons {"Cancel", "OK"} default button 2 with icon {"/path/to/file.png"})' )
+	#check status of osascript, if user clicked cancel exit script..
 	if [ "$?" != "0" ] ; then
 		echo "User aborted. Exiting..."
 		exit 1
@@ -23,23 +28,17 @@ until [[ "$username" =~ ^[A-Z][a-z]+[[:space:]]+[A-Z][a-z]+$ ]]; do
 done
 
 #get config profile id
-configID=$(curl -s -H '' -X GET "$jamfProURL/osxconfigurationprofiles" -H "accept: application/xml" | xmllint --format - | awk -F '[<>]' '/<id>/{print $3}')
+configID=$(curl -s -H 'Authorization: Basic '$jamfcreds'' -X GET "$jamfProURL/osxconfigurationprofiles" -H "accept: application/xml" | xmllint --format - | awk -F '[<>]' '/<id>/{print $3}')
 
 #get config profile name
 for id in $configID
 do
-	config_name=$(curl -s -H '' -X GET "$jamfProURL/osxconfigurationprofiles/id/$id" -H "accept: application/xml" | xmllint --xpath '/os_x_configuration_profile/general/name/text()' - )
+	config_name=$(curl -s -H 'Authorization: Basic '$jamfcreds'' -X GET "$jamfProURL/osxconfigurationprofiles/id/$id" -H "accept: application/xml" | xmllint --xpath '/os_x_configuration_profile/general/name/text()' - )
 	
 	#remove config profile 
-	if [[ $config_name = "VPNAZN - $username" ]]; then 
+	if [[ $config_name = "VPN - $username" ]]; then 
 		echo "Removing $config_name"
-		curl -s -H '' "$jamfProURL/osxconfigurationprofiles/id/$id" -X DELETE
-	elif [[ $config_name = "VPNFUL- $username" ]]; then 
-			echo "Removing $config_name"
-			curl -s -H '' "$jamfProURL/osxconfigurationprofiles/id/$id" -X DELETE
-		elif [[ $config_name = "VPNHQ - $username" ]]; then 
-			echo "Removing $config_name"
-			curl -s -H '' "$jamfProURL/osxconfigurationprofiles/id/$id" -X DELETE
+		curl -s -H 'Authorization: Basic '$jamfcreds'' "$jamfProURL/osxconfigurationprofiles/id/$id" -X DELETE
 	fi
 done
 
